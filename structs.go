6546aa077079609dcecc1c344dd42f66ac526061
Alexander Staubo <alex@bengler.no>
2016-05-23 12:05:23 -0400

    Add Organizations to Organization.

--- structs.go
+++ structs.go
@@ -3,13 +3,14 @@ package centralclient
 import "time"
 
 type Organization struct {
-	Id        int        `json:"id"`
-	Title     string     `json:"title"`
-	ParentId  int        `json:"parent_id"`
-	Path      string     `json:"path"`
-	Realm     string     `json:"realm"`
-	CreatedAt *time.Time `json:"created_at"`
-	UpdatedAt *time.Time `json:"updated_at"`
+	Id            int             `json:"id"`
+	Title         string          `json:"title"`
+	ParentId      int             `json:"parent_id"`
+	Path          string          `json:"path"`
+	Realm         string          `json:"realm"`
+	CreatedAt     *time.Time      `json:"created_at"`
+	UpdatedAt     *time.Time      `json:"updated_at"`
+	Organizations []*Organization `json:"organizations"`
 }
 
 type Application struct {
